a8df5e980691cd7197054112fe398ec49230b5b5
Salem Lokmani <salem.lokmani99@gmail.com>
2026-06-15 00:13:54 +0100

    Add `resetAll` endpoint to delete all feedback

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,POST,OPTIONS',
+    methods: 'GET,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
